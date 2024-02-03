759f40d44b1e16b479bbbcab5e940226f14e4eef
Jose Manuel Perez Carrasco <33356314+roulez@users.noreply.github.com>
2024-02-03 12:51:40 +0100

    Created files for the database creation and insert of some sample data

---
+++ people-among-dragons/database/scripts/create_tables.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Users {
+    Id int NOT NULL AUTO_INCREMENT,
+    UserEmail varchar(255) NOT NULL,
+    UserPassword varchar(255) NOT NULL,
+    UserName varchar(255) NOT NULL,
+    UserSurname varchar(255) NOT NULL,
+    PRIMARY KEY (Id)
+};
+
+CREATE TABLE Publications {
+    Id int NOT NULL AUTO_INCREMENT,
+    PublicationTitle varchar(255) NOT NULL,
+    PublicationText varchar(255) NOT NULL,
+    PublicationImage varchar(255),
+    UserId int NOT NULL,
+    CreationDate DATETIME NOT NULL,
+    PRIMARY KEY (Id),
+    FOREIGN KEY (UserId) REFERENCES Users(Id)
+};
+
+CREATE TABLE Campaigns {
+    Id int NOT NULL AUTO_INCREMENT,
+    CampaignName varchar(255) NOT NULL,
+    CampaignImage varchar(255),
+    UserId int NOT NULL,
+    CreationDate DATETIME NOT NULL,
+    PRIMARY KEY (Id),
+    FOREIGN KEY (UserId) REFERENCES Users(Id)
+};
+
+CREATE TABLE CampaignsUsers {
+    Id int NOT NULL AUTO_INCREMENT,
+    UserId int NOT NULL,
+    CampaignId int NOT NULL,
+    PRIMARY KEY (Id),
+    FOREIGN KEY (UserId) REFERENCES Users(Id),
+    FOREIGN KEY (CampaignId) REFERENCES Campaigns(Id)
+};
+
+CREATE TABLE Messages {
+    Id int NOT NULL AUTO_INCREMENT,
+    MessageTitle varchar(255) NOT NULL,
+    MessageText varchar(255) NOT NULL,
+    SendingUserId int NOT NULL,
+    ReceivingUserId int NOT NULL,
+    CreationDate DATETIME NOT NULL,
+    PRIMARY KEY (Id),
+    FOREIGN KEY (SendingUserId) REFERENCES Users(Id),
+    FOREIGN KEY (ReceivingUserId) REFERENCES Users(Id)
+};
---
+++ people-among-dragons/database/scripts/insert_data.sql
@@ -0,0 +1,21 @@
+--Insert Sample Users
+INSERT INTO Users (UserEmail, UserPassword, UserName, UserSurname) Values ('jmpc@gmail.com', '123456789', 'Jose Manuel', 'Perez Carrasco');
+INSERT INTO Users (UserEmail, UserPassword, UserName, UserSurname) Values ('user@gmail.com', 'appuser', 'user', 'user');
+
+--Insert Sample Publications
+INSERT INTO Publications (PublicationTitle, PublicationText, PublicationImage, UserId, CreationDate) Values ('Test Publication 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus mi a erat cursus malesuada. Nullam vel ligula nisi. Maecenas dictum aliquet dui nec lacinia. Aenean posuere elit tellus, nec aliquam urna porttitor sit amet. Fusce vitae dolor dictum, cursus eros vel, luctus nulla. Fusce suscipit justo vel sem fermentum convallis. In elementum nisi ac placerat ullamcorper. Nullam eget faucibus est. Morbi luctus lectus dolor, quis cursus tortor finibus sed. Phasellus aliquam dictum facilisis. Nulla facilisis enim sit amet ante dapibus, vel vestibulum sem venenatis. Nam commodo mauris nec laoreet feugiat.', '../../../assets/images/login-screen.jpg', 1, CURRENT_TIMESTAMP);
+INSERT INTO Publications (PublicationTitle, PublicationText, PublicationImage, UserId, CreationDate) Values ('Test Publication 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus mi a erat cursus malesuada. Nullam vel ligula nisi. Maecenas dictum aliquet dui nec lacinia. Aenean posuere elit tellus, nec aliquam urna porttitor sit amet. Fusce vitae dolor dictum, cursus eros vel, luctus nulla. Fusce suscipit justo vel sem fermentum convallis. In elementum nisi ac placerat ullamcorper. Nullam eget faucibus est. Morbi luctus lectus dolor, quis cursus tortor finibus sed. Phasellus aliquam dictum facilisis. Nulla facilisis enim sit amet ante dapibus, vel vestibulum sem venenatis. Nam commodo mauris nec laoreet feugiat.', '../../../assets/images/login-screen.jpg', 2, CURRENT_TIMESTAMP);
+
+--Insert Sample Campaigns
+INSERT INTO Campaigns (CampaignName, CampaignImage, UserId, CreationDate) Values ('Test Campaign 1', '../../../assets/images/login-screen.jpg', 1, CURRENT_TIMESTAMP);
+INSERT INTO Campaigns (CampaignName, CampaignImage, UserId, CreationDate) Values ('Test Campaign 2', '../../../assets/images/login-screen.jpg', 2, CURRENT_TIMESTAMP);
+
+--Insert Sample User-Campaign relationships
+INSERT INTO CampaignsUsers (UserId, CampaignId) Values (1, 1);
+INSERT INTO CampaignsUsers (UserId, CampaignId) Values (2, 2);
+INSERT INTO CampaignsUsers (UserId, CampaignId) Values (1, 2);
+
+--Insert Sample Messages
+INSERT INTO Messages (MessageTitle, MessageText, SendingUserId, ReceivingUserId, CreationDate) Values ("First Message!", "This is my first message", 2, 1, CURRENT_TIMESTAMP);
+INSERT INTO Messages (MessageTitle, MessageText, SendingUserId, ReceivingUserId, CreationDate) Values ("First Message!", "This is my first message", 1, 2, CURRENT_TIMESTAMP);
+INSERT INTO Messages (MessageTitle, MessageText, SendingUserId, ReceivingUserId, CreationDate) Values ("Second Message!", "This is my second message", 2, 1, CURRENT_TIMESTAMP);
