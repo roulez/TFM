4d3bee07edfef57156e420dac466cb930b60f6b9
Jose Manuel Perez Carrasco <33356314+roulez@users.noreply.github.com>
2024-02-04 12:13:47 +0100

    Fixed issues on the Create Tables sql script

--- people-among-dragons/database/scripts/create_tables.sql
+++ people-among-dragons/database/scripts/create_tables.sql
@@ -1,24 +1,24 @@
-CREATE TABLE Users {
+CREATE TABLE Users (
     Id int NOT NULL AUTO_INCREMENT,
     UserEmail varchar(255) NOT NULL,
     UserPassword varchar(255) NOT NULL,
     UserName varchar(255) NOT NULL,
     UserSurname varchar(255) NOT NULL,
     PRIMARY KEY (Id)
-};
+);
 
-CREATE TABLE Publications {
+CREATE TABLE Publications (
     Id int NOT NULL AUTO_INCREMENT,
     PublicationTitle varchar(255) NOT NULL,
-    PublicationText varchar(255) NOT NULL,
+    PublicationText TEXT NOT NULL,
     PublicationImage varchar(255),
     UserId int NOT NULL,
     CreationDate DATETIME NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (UserId) REFERENCES Users(Id)
-};
+);
 
-CREATE TABLE Campaigns {
+CREATE TABLE Campaigns (
     Id int NOT NULL AUTO_INCREMENT,
     CampaignName varchar(255) NOT NULL,
     CampaignImage varchar(255),
@@ -26,18 +26,18 @@ CREATE TABLE Campaigns {
     CreationDate DATETIME NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (UserId) REFERENCES Users(Id)
-};
+);
 
-CREATE TABLE CampaignsUsers {
+CREATE TABLE CampaignsUsers (
     Id int NOT NULL AUTO_INCREMENT,
     UserId int NOT NULL,
     CampaignId int NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (UserId) REFERENCES Users(Id),
     FOREIGN KEY (CampaignId) REFERENCES Campaigns(Id)
-};
+);
 
-CREATE TABLE Messages {
+CREATE TABLE Messages (
     Id int NOT NULL AUTO_INCREMENT,
     MessageTitle varchar(255) NOT NULL,
     MessageText varchar(255) NOT NULL,
@@ -47,4 +47,4 @@ CREATE TABLE Messages {
     PRIMARY KEY (Id),
     FOREIGN KEY (SendingUserId) REFERENCES Users(Id),
     FOREIGN KEY (ReceivingUserId) REFERENCES Users(Id)
-};
+);
