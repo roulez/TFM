0f8ee956886dea0dd815c93416af46e89326e453
Jose Manuel Perez Carrasco <33356314+roulez@users.noreply.github.com>
2024-02-09 11:55:29 +0100

    Added gitignore

--- Dragon-WebApi/obj/Debug/net8.0/Dragon-WebApi.AssemblyInfo.cs
+++ Dragon-WebApi/obj/Debug/net8.0/Dragon-WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dragon-WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271e7442d9f127c87f88d5a3bcc2843fcabb9906")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c52dac86d1da59c5aacb9d421ef509e2cb48e908")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dragon-WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dragon-WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
